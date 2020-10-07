c46bde0115921d56a01289b1a78d1efcebc500c6
MuchiriKahwai <64806813+MuchiriKahwai@users.noreply.github.com>
2020-10-07 14:53:11 +0300

    Muchiri edited this

--- js/game.js
+++ js/game.js
@@ -3,13 +3,32 @@ d3. select ("#BringSuspects") . on( "click", function()
        .classed( "hidden",false);
  }
  )
+d3. select("#Suspectbox") .on ("click", function() 
+{d3.select ("#Suspectbox0")
+    .classed("hidden", false) ;
+}
+)
+d3. select("#Suspectbox0") .on ("mouseenter", function() 
+{d3.select ("#Suspectbox1")
+    .classed("hidden", false);
+}
+)
 
+d3. select("#Suspectbox1") .on ("mouseenter", function() 
+{d3.select ("#Suspectbox2")
+    .classed("hidden", false);
+}
+)
+d3. select("#Suspectbox2") .on ("mouseenter", function() 
+{d3.select ("#Suspectbox3")
+    .classed("hidden", false);
+}
+)
 
-d3. select ("#PhotoDouglass") . on( "click", function()
-{ d3   .select("#banner")
-         .text( "Not today");
+ d3. select("#Suspectbox3") .on ("mouseenter", function() 
+{d3.select ("#empty")
+    .classed("hidden", false);
+}
+)
+
  
- }
- )
-       
-                                   
